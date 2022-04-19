e8cd6de346a45ca3ca91532f3caf5a76b1921061
i-vasilev <ilya.vasilev.vladislavovich@gmail.com>
2022-04-20 00:24:55 +0300

    Добавлена реализация предобработки и классификации нейронки

--- app/src/main/java/ru/vasilev/mlplayer/MainActivity.java
+++ app/src/main/java/ru/vasilev/mlplayer/MainActivity.java
@@ -6,12 +6,15 @@ import static ru.vasilev.mlplayer.R.id.play_pause;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
@@ -21,14 +24,22 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import be.tarsos.dsp.io.android.AndroidFFMPEGLocator;
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
+import cafe.adriel.androidaudioconverter.callback.ILoadCallback;
+import ru.vasilev.mlplayer.ml.Model;
 import ru.vasilev.mlplayer.nn.Preprocessor;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,11 +49,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mediaPlayer = new MediaPlayer();
+        AndroidAudioConverter.load(this, new ILoadCallback() {
+            @Override
+            public void onSuccess() {
+                // Great!
+            }
+
+            @Override
+            public void onFailure(Exception error) {
+                // FFmpeg is not supported by device
+            }
+        });
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         findViewById(openFile).setOnClickListener(this::clickBtn);
         findViewById(play_pause).setOnClickListener(this::clickBtn);
         findViewById(openEqualizer).setOnClickListener(this::clickBtn);
+    }
+
+    private void openFile(Uri pickerInitialUri) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+
+        // Optionally, specify a URI for the file that should appear in the
+        // system file picker when it loads.
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, 1);
     }
 
     private void clickBtn(View view) {
@@ -51,9 +86,15 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this,
                                                   new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                                                   PERMISSION_EXTERNAL_STORAGE);
-                OpenFileDialog openFileDialog = new OpenFileDialog(MainActivity.this)
-                        .setOpenDialogListener(this::openSong);
-                openFileDialog.show();
+//                OpenFileDialog openFileDialog = new OpenFileDialog(MainActivity.this)
+//                        .setOpenDialogListener(this::openSong);
+//                openFileDialog.show();
+
+                Intent intent = new Intent()
+                        .setType("*/*")
+                        .setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(intent, "Select a file"), 123);
                 break;
             case play_pause:
                 if (mediaPlayer != null) {
@@ -65,20 +106,22 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case openEqualizer:
-                Intent intent = new Intent(getApplicationContext(), EqualizerActivity.class);
+                intent = new Intent(getApplicationContext(), EqualizerActivity.class);
                 startActivity(intent);
                 break;
         }
     }
 
-    private void openSong(String fileName) {
-        mediaPlayer = new MediaPlayer();
+    private void openSong(AssetFileDescriptor fileName) {
         try {
+            ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+            mediaPlayer.stop();
+
+            mediaPlayer = new MediaPlayer();
             mediaPlayer.setDataSource(fileName);
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
             mediaPlayer.prepare();
             initLayout(fileName);
-            ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
             Runnable task = () -> {
                 ProgressBar progressBar = findViewById(R.id.progressBar);
                 progressBar.setProgress(mediaPlayer.getCurrentPosition());
@@ -86,21 +129,37 @@ public class MainActivity extends AppCompatActivity {
                 currentPosition.setText(getFormattedTime(mediaPlayer.getCurrentPosition()));
             };
             new AndroidFFMPEGLocator(this);
-//            AudioClassifier.AudioClassifierOptions options =
-//                    AudioClassifier.AudioClassifierOptions.builder()
-//                                                          .setBaseOptions(BaseOptions.builder()
-//                                                                                     .useGpu()
-//                                                                                     .build())
-//                                                          .setMaxResults(1)
-//                                                          .build();
-////            org.tensorflow.lite.support.audio.TensorAudio.create(AudioFormat.ENCODING_MP3, )
+
+
+            Preprocessor preprocessor = new Preprocessor();
+            double[] preprocess = preprocessor.preprocess(fileName.getFileDescriptor());
+            float[] a = new float[preprocess.length];
+            for (int i = 0; i < preprocess.length; i++) {
+                a[i] = (float) preprocess[i];
+            }
+//            float[][] a = preprocessor.preprocess(fileName.getFileDescriptor());
+            try {
+                Model model = Model.newInstance(getApplicationContext());
+
+                // Creates inputs for reference.
+                TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 64, 173, 1}, DataType.FLOAT32);
+                inputFeature0.loadArray(a);
+
+                // Runs model inference and gets result.
+                Model.Outputs outputs = model.process(inputFeature0);
+                TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                // Releases model resources if no longer used.
+                model.close();
+            } catch (IOException e) {
+                // TODO Handle the exception
+            }
+
+//            org.tensorflow.lite.support.audio.TensorAudio.create(AudioFormat.ENCODING_MP3, )
 //            AudioClassifier classifier =
 //                    AudioClassifier.createFromFileAndOptions(getApplicationContext(), "model.tflite", options);
 //            TensorAudio inputTensorAudio = classifier.createInputTensorAudio();
-//            inputTensorAudio.load();
-
-            Preprocessor preprocessor = new Preprocessor();
-            preprocessor.preprocess(fileName);
+//            inputTensorAudio.load(a);
 
             scheduledExecutorService.scheduleAtFixedRate(task, 0, 500, TimeUnit.MILLISECONDS);
             mediaPlayer.start();
@@ -116,9 +175,9 @@ public class MainActivity extends AppCompatActivity {
         return dateFormat.format(date);
     }
 
-    private void initLayout(String fileName) {
+    private void initLayout(AssetFileDescriptor fileName) {
         MediaMetadataRetriever metadataRetriever = new MediaMetadataRetriever();
-        metadataRetriever.setDataSource(fileName);
+        metadataRetriever.setDataSource(fileName.getFileDescriptor());
         setImage(metadataRetriever);
         String artist = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
         String title = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
@@ -149,5 +208,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123 && resultCode == RESULT_OK) {
+            Uri selectedfile = data.getData(); //The uri with the location of the file
+            try {
+                AssetFileDescriptor assetFileDescriptor = getApplicationContext().getContentResolver()
+                                                               .openAssetFileDescriptor(selectedfile, "r");
+                openSong(assetFileDescriptor);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/ru/vasilev/mlplayer/nn/Preprocessor.java
+++ app/src/main/java/ru/vasilev/mlplayer/nn/Preprocessor.java
@@ -1,16 +1,15 @@
 package ru.vasilev.mlplayer.nn;
 
+import android.content.Context;
 import android.util.Log;
 
-
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
@@ -18,9 +17,32 @@ import be.tarsos.dsp.AudioProcessor;
 import be.tarsos.dsp.io.TarsosDSPAudioFormat;
 import be.tarsos.dsp.io.UniversalAudioInputStream;
 import be.tarsos.dsp.mfcc.MFCC;
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
+import cafe.adriel.androidaudioconverter.callback.IConvertCallback;
+import cafe.adriel.androidaudioconverter.model.AudioFormat;
 
 public class Preprocessor {
-    public void preprocess(String resource) throws IOException {
+    public void convertToMp3(String resource, Context context) {
+        File file = new File(resource);
+        IConvertCallback callback = new IConvertCallback() {
+            @Override
+            public void onSuccess(File convertedFile) {
+                Log.d("converter", "CONVERTED");
+            }
+
+            @Override
+            public void onFailure(Exception error) {
+                Log.e("converter", "failed", error);
+            }
+        };
+        AndroidAudioConverter.with(context)
+                             .setFile(file)
+                             .setFormat(AudioFormat.WAV)
+                             .setCallback(callback)
+                             .convert();
+    }
+
+    public double[] preprocess(FileDescriptor resource) throws IOException {
         int sampleRate = 44100;
         //set parameters, And these are same as the python librosa library
         //window size
@@ -29,9 +51,9 @@ public class Preprocessor {
         int bufferOverlap = bufferSize - 512;
         int fmin = 30;
         int fmax = 3000;//(int) (sampleRate*0.5);
-        int n_cep_mel = 40;
+        int n_cep_mel = 64;
         int n_mels = 128;
-        List<float[]> mfccList = new ArrayList<>();
+        final double[][] mfccList = new double[173][];
         try (InputStream is = new FileInputStream(resource)) {
 
             AudioDispatcher dispatcher = new AudioDispatcher(
@@ -42,6 +64,7 @@ public class Preprocessor {
                     ),
                     bufferSize,
                     bufferOverlap);
+            final int[] k = {0, 0};
             final MFCC mfcc = new MFCC(bufferSize, sampleRate, n_cep_mel, n_mels, fmin,
                                        fmax);
             dispatcher.addAudioProcessor(mfcc);
@@ -54,20 +77,32 @@ public class Preprocessor {
 
                 @Override
                 public boolean process(AudioEvent audioEvent) {
-                    //fetchng MFCC array and removing the 0th index because its energy coefficient and florian asked to discard
-                    float[] mfccOutput = mfcc.getMFCC();
-                    mfccOutput = Arrays.copyOfRange(mfccOutput, 0,
-                                                    mfccOutput.length);
-
-                    mfccList.add(mfccOutput);
+                    if (k[1] > 200) {
+                        float[] mfccOutput = mfcc.getMFCC();
+                        mfccOutput = Arrays.copyOfRange(mfccOutput, 0,
+                                                        mfccOutput.length);
+                        double[] mfccDouble = new double[mfccOutput.length];
+                        for (int i = 0; i < mfccOutput.length; i++) {
+                            mfccDouble[i] = mfccOutput[i];
+                        }
+                        mfccList[k[0]++] = mfccDouble;
+                        if (k[0] == 173)
+                            dispatcher.stop();
+                    } else {
+                        k[1]++;
+                    }
                     return true;
                 }
             });
             dispatcher.run();// starts a new thread
-            System.out.println(mfccList.size());
+
+            return Arrays.stream(mfccList)
+                         .flatMapToDouble(Arrays::stream)
+                         .toArray();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return new double[0];
     }
 
 }
